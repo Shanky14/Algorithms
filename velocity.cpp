e7aa2ab8efa475b29974f1f0a4c74a28941815ba
Kaushal Shushank <kaushalshushank2@gmail.com>
2026-01-27 21:03:55 +0530

    Changes made

--- velocity.cpp
+++ velocity.cpp
@@ -1,15 +1,18 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int findTips(double v)
+int findTips(double v,int tips)
 {
-    int tips=0;
-    if(v<1.0)
-    return 0;
-
-    return 1+ findTips(v-0.425);
+    if (v >= 1.0)
+    {
+        tips++;
+        v = v - (0.425 * v);
+        return findTips(v,tips);
+    }
+    else
+        return tips;
 }
 int main()
 {
-    cout<<"no of tips : "<<findTips(5)<<endl;
+    cout << "no of tips : " << findTips(40,0) << endl;
     return 0;
 }
