af79ddfdb89886db18c536c3c0ed1f616ec476dc
Kaushal Shushank <kaushalshushank2@gmail.com>
2026-04-21 22:17:38 +0530

    Allpair shortest

--- lab8/allPairShortest.cpp
+++ lab8/allPairShortest.cpp
@@ -1,2 +1,43 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+const int INF = 1e9;
+
+void allpairShort(vector<vector<int>>& dist, int n) {
+    for (int k = 0; k < n; k++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (dist[i][k] < INF && dist[k][j] < INF) {
+                    dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+                }
+            }
+        }
+    }
+}
+
+int main() {
+    int n = 4;
+
+    vector<vector<int>> dist = {
+        {0, 3, INF, 7},
+        {8, 0, 2, INF},
+        {5, INF, 0, 1},
+        {2, INF, INF, 0}
+    };
+
+    allpairShort(dist, n);
+
+    cout << "Shortest distance matrix:\n";
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (dist[i][j] == INF)
+                cout << "INF ";
+            else
+                cout << dist[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
